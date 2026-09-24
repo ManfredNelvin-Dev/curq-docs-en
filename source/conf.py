0f71d54629323a57fae19eae6a30e98910536f0f
ManfredNelvin <manfrednelvin@gmail.com>
2026-09-24 20:43:16 +0530

    Add dynamic language switcher

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,39 @@ gettext_compact = False
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_sidebars = {
+    '**': [
+        'versions.html',
+    ]
+}
+
+import os
+try:
+    from babel import Locale
+except ImportError:
+    Locale = None
+
+locale_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+
+def get_language_name(code):
+    if Locale:
+        try:
+            return Locale.parse(code).get_display_name(code).capitalize()
+        except Exception:
+            pass
+    return code.upper()
+
+dynamic_languages = [('en', get_language_name('en'))]
+
+if os.path.exists(locale_dir):
+    for code in os.listdir(locale_dir):
+        if os.path.isdir(os.path.join(locale_dir, code)) and code != 'en':
+            dynamic_languages.append((code, get_language_name(code)))
+
+html_context = {
+    'supported_languages': dynamic_languages
+}
+
 smv_tag_whitelist = r'^.*$'
-smv_branch_whitelist = r'^.*$'
+smv_branch_whitelist = r'^(18\.0|19\.0|main)$'
 smv_remote_whitelist = r'^origin$'
